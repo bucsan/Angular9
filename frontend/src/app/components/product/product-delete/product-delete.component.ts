f140eca3ad9e43b3a2bb863e88353a7b9187bd74
Thiago Bucsan <thiago.bucsan@gmail.com>
2020-07-31 19:30:26 -0300

    Desafio Componente Excluir Produto
    - Alteração do tipo do parâmetro da rota.

--- frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -4,17 +4,18 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../product.model';
 
 @Component({
-  selector: 'app-product-update',
-  templateUrl: './product-update.component.html',
-  styleUrls: ['./product-update.component.css']
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
 })
-export class ProductUpdateComponent implements OnInit {
+export class ProductDeleteComponent implements OnInit {
   product: Product;
+
   constructor(
-    private productService: ProductService, 
+    private productService: ProductService,
     private router: Router,
     private route: ActivatedRoute
-    ) { }
+  ) { }
 
   ngOnInit(): void {
     const id = +this.route.snapshot.paramMap.get('id');
@@ -23,15 +24,14 @@ export class ProductUpdateComponent implements OnInit {
     });
   }
 
-  updateProduct(): void {
-    this.productService.update(this.product).subscribe(() => {
-      this.productService.showMessage('Produto atualizado com sucesso!');
+  deleteProduct(): void {
+    this.productService.delete(this.product.id).subscribe(() => {
+      this.productService.showMessage('Produto excluído com sucesso!');
       this.router.navigate(['/products']);
-    })
+    });
   }
 
   cancel(): void {
     this.router.navigate(['/products']);
   }
-
 }
